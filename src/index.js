6bdd9ce7e2315763225cf70dfd355554f2698ccb
wang1dot0 <215000482@qq.com>
2019-01-30 22:43:18 +0800

    Rename config file and loading css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   var ele = document.createElement('div');
   ele.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  ele.classList.add('hello');
 
   return ele;
 }
